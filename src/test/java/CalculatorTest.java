c4583b257e4d08617d13205bcb4a08c0b0e93c6a
carina-codrea <codrecarina@yahoo.com>
2024-11-11 19:10:19 +0200

    removed tests for assignment from CalculatorTest class

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -67,60 +67,6 @@ public class CalculatorTest {
         Assertions.assertEquals(Math.sqrt(2),calculator.compute(2,0,"SQRT"));
     }
 
-    //assignment tests
-
-    @Test
-    public void additionWithNegativeOperandTest(){
-        Assertions.assertEquals(15,calculator.compute(-27,42,"+"),"Addition with negative operand failed.");
-    }
-
-    @Test
-    public void additionWithNegativeOperandsTest(){
-        Assertions.assertEquals(-802.4,calculator.compute(-234.5,-567.9,"+"),"Addition with negative operands failed.");
-    }
-
-    @Test
-    public void additionWithDoubleMaxValueTest(){
-        Assertions.assertEquals(Double.MAX_VALUE,calculator.compute(Double.MAX_VALUE,20,"+"),"Addition with DOUBLE MAX VALUE failed.");
-    }
-
-    @Test
-    public void subtractionWithNegativeOperandsTest(){
-        Assertions.assertEquals(-13.300000000000004,calculator.compute(-76.4,-63.1,"-"),"Subtraction with negative operands failed");
-
-    }
-
-    @Test
-    public void subtractionWithPositiveOperandsTest(){
-        Assertions.assertEquals(36,calculator.compute(56,20,"-"),"Subtraction with positive operands failed.");
-
-    }
-
-    @Test
-    public void divisionByNegativeOperandTest(){
-        Assertions.assertEquals(-3.5,calculator.compute(7,-2,"/"),"Division by negative operand failed.");
-    }
-
-    @Test
-    public void divisionByNegativeOperandsTest(){
-        Assertions.assertEquals(1.570796460176991,calculator.compute(-10.65,-6.78,"/"),"Division by negative operands failed.");
-    }
-
-    @Test
-    public void divisionByPositiveOperandsTest(){
-        Assertions.assertEquals(32.09489361702128,calculator.compute(754.23,23.5,"/"),"Division by positive operands failed.");
-    }
-
-    @Test
-    public void divisionBySmallNumberTest(){
-        Assertions.assertEquals(1533999.9999999998,calculator.compute(15.34,1e-5,"/"));
-    }
-
-    @Test
-    public void sqrtOfNegativeNumber(){
-        Assertions.assertEquals(NaN,calculator.compute(-64,0,"SQRT"));
-    }
-
 
 
     @AfterEach
